ede9fde132bc880cbc0f7b6d94a5b584e835666d
Rizal muhamad H <rizalmuhamadh@gmail.com>
2021-04-23 18:06:43 +0700

    Initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <nav class="bg-gray-800">
+      <div
+        class="flex flex-col max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 items-center"
+      >
+        <div class="flex flex-col justify-center fixed z-40">
+          <img
+            src="https://ayobogor.com/assets/ads/bjb/desktop/ads.jpg?w=1197"
+            alt="bjb"
+          />
+          <transition name="fade" mode="out-in" leave-cancelled>
+            <NavItem v-show="position" />
+          </transition>
+        </div>
+        <div class="flex flex-col pt-28 pb-1">
+          <img src="~/assets/logo-ayobandung.png" alt="" />
+          <h1 class="text-lg text-white text-center py-2">
+            Semua Tentang Bandung
+          </h1>
+        </div>
+      </div>
+      <div class="w-full border-t border-white">
+      <NavItem />
+      </div>
+
+      <!-- Mobile menu, show/hide based on menu state. -->
+      <div class="sm:hidden" id="mobile-menu">
+        <div class="px-2 pt-2 pb-3 space-y-1">
+          <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+          <a
+            href="#"
+            class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+            aria-current="page"
+            >Dashboard</a
+          >
+
+          <a
+            href="#"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >Team</a
+          >
+
+          <a
+            href="#"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >Projects</a
+          >
+
+          <a
+            href="#"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >Calendar</a
+          >
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      position: false,
+    }
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      // Any code to be executed when the window is scrolled
+      if (window.scrollY >= 200) {
+        this.position = true
+      } else {
+        this.position = false
+      }
+      console.log(this.position)
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="text-gray-400 text-xl">base-frontend-desktop</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+    <template v-if="isDev"> [ADSENSE PLACEHOLDER] </template>
+    <ins
+      class="adsbygoogle"
+      style="display: block"
+      data-ad-client="ca-pub-5267702858942303"
+      data-ad-slot="2639233875"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  </div>
+</template>
+
+<script>
+// https://code.luasoftware.com/tutorials/nuxtjs/nuxtjs-manual-adsense-component/
+export default {
+  head: {
+    script: [
+      {
+        hid: 'adsense',
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+        defer: true,
+        'data-ad-client': 'ca-pub-5267702858942303',
+        sync: true,
+      },
+    ],
+  },
+  data() {
+    return {
+      isDev: process.env.NODE_ENV !== 'production',
+    }
+  },
+  mounted() {
+    if (!this.isDev) {
+      this.$nextTick(() => {
+        try {
+          // this is required for each ad slot (calling this once will only load 1 ad)
+          ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+        } catch (error) {
+          console.error(error)
+        }
+      })
+    }
+  },
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
